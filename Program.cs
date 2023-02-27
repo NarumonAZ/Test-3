b3e96504975f0125be4ec683d76a7829bde10808
AZ <Narumonokanoto@gmail.com>
2023-02-27 11:20:45 +0700

    3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main(string[] args) {
+        int sum, add, multiply;
+
+            for (sum = 0, add = 1,multiply = 1;
+                add <= 10;
+                add++, multiply *=2) {
+                    sum += add * multiply
+            }
+             Console.WriteLine("Summary is {0}",sum);
+
+
+    }
+}
