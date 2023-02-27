dfa8426591f01c265a3595fc8c3a1257ef2e462f
AZ <Narumonokanoto@gmail.com>
2023-02-27 11:39:49 +0700

    9

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
     static void Main(string[] args) {
         for (int row = 1; row <= 5; row++) {
             for (int col = 1; col <= 6; col++) {
-               Console.WriteLine("({0}, {1}) ", row, col); 
+               Console.Write("({0}, {1}) ", row, col); 
 
             }
 
